78479d29412d936080cdce58b6387d5020bf3f8e
Sebastian Balzer <sebastian.balzer@gmx.ch>
2024-02-07 15:08:28 +0100

    use enlighten for progress bars

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import hydra
+import enlighten
 
 import numpy as np
 import pandas as pd
 from loguru import logger
 from omegaconf import OmegaConf
-from tqdm import tqdm
 
 from helpers.helpers import numpy_range
 from preprocessing.preprocessing import Preprocessing
@@ -19,13 +19,15 @@ def main(config):
     if config.meta.out_file is None:
             in_path = os.path.splitext(config.meta.in_file)[0]  # remove extension
             config.meta.out_file = f'{in_path}_results.xlsx'
+    progress_manager = enlighten.get_manager()
+    pbar = progress_manager.counter(total=config.meta.n_seeds, desc='Seeds', unit='seeds')
     np.random.seed(config.meta.init_seed)
     seeds = np.random.randint(low=0, high=2**32, size=config.meta.n_seeds)  # generate desired number of random seeds
     preprocessing = Preprocessing(config)
-    pipeline = Survival(config)
+    pipeline = Survival(config, progress_manager)
     report = Report(config)
 
-    for seed in tqdm(seeds, desc='Seeds'):
+    for seed in seeds:
         np.random.seed(seed)
         data_x_train, data_x_test, data_y_train, data_y_test = preprocessing(seed)
         results = pipeline(
@@ -35,7 +37,9 @@ def main(config):
             data_x_test,
             data_y_test,
         )
+        pbar.update()
 
+    pbar.close()
     report(results)
     pd.options.display.float_format = '{:.3f}'.format
     logger.info(f'\n{results}')
